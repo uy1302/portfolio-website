7971682b0b6acbcdf09b351503d808694e5f61ba
uy1302 <134634435+uy1302@users.noreply.github.com>
2023-05-26 00:19:52 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//toggle icon navbar
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+//scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
